826a87b7d9ab85cae7dfef1ebe80ec4bb0393a8e
ambs <ambs@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2009-05-02 15:19:32 +0000

    Menos chaos, ainda muita confusao.
    git-svn-id: https://natura.di.uminho.pt/svn/main/Text/BibTeX@7266 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- scripts/build.pl
+++ scripts/build.pl
@@ -8,6 +8,12 @@ use Config::AutoConf;
 use ExtUtils::CBuilder;
 
 my $VERSION = get_version();
+my $EXE = "";
+$EXE = ".exe" if $^O eq "MSWin32";
+my $LIBEXT = ".so";
+$LIBEXT = ".bundle" if $^O =~ /darwin/i;
+$LIBEXT = ".dll"   if $^O =~ /mswin32/i;
+
 # Show some information to the user about what are we doing.
 print "\n - Building btparse ($VERSION) - \n";
 
@@ -27,7 +33,7 @@ if (!$make) {
 }
 
 ## Build PODs
-print "Building manpages...\n";
+print "\nBuilding manpages...\n";
 my @pods = <btparse/doc/*.pod>;
 for my $pod (@pods) {
     my $man = $pod;
@@ -37,9 +43,33 @@ for my $pod (@pods) {
     move($man, 'blib/man3/');
 }
 
+## Build libbtparse
+print "\nCompiling libbtparse...\n";
 my $CC = ExtUtils::CBuilder->new(quiet => 0);
 
+my @sources = qw:init.c input.c bibtex.c err.c scan.c error.c
+                 lex_auxiliary.c parse_auxiliary.c bibtex_ast.c sym.c
+                 util.c postprocess.c macros.c traversal.c modify.c
+                 names.c tex_tree.c string_util.c format_name.c:;
 
+my @objects = map {
+    $CC->compile(include_dirs => ['btparse/pccts'],
+                 source => "btparse/src/$_" )
+} @sources;
+
+$CC->link(module_name => 'btparse',
+          objects => \@objects,
+          lib_file => "btparse/src/libbtparse$LIBEXT");
+
+## Build dumpnames
+print "\nCompiling dumpnames application...\n";
+@sources = qw!dumpnames.c!;
+@objects =  map {
+    $CC->compile(include_dirs => ['btparse/src'],
+                 source => "btparse/progs/$_" )
+} @sources;
+$CC->link_executable(exe_file => "btparse/progs/dumpnames$EXE",
+                     objects => ["btparse/src/libbtparse$LIBEXT",@objects]);
 
 sub get_version {
     my $version = undef;
