9283a942bfd5738c5885d43dcc3291bfdd2bb0ed
ambs <ambs@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2010-03-08 21:39:48 +0000

    freebsdOK
    git-svn-id: https://natura.di.uminho.pt/svn/main/Text/BibTeX@8512 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- inc/MyBuilder.pm
+++ inc/MyBuilder.pm
@@ -264,7 +264,7 @@ sub ACTION_test {
     if ($^O =~ /darwin/i) {
         $ENV{DYLD_LIBRARY_PATH} = catdir($self->blib,"usrlib").":$ENV{DYLD_LIBRARY_PATH}";
     }
-    if ($^O =~ /(solaris|linux)/i) {
+    if ($^O =~ /(freebsd|solaris|linux)/i) {
         $ENV{LD_LIBRARY_PATH} = catdir($self->blib,"usrlib").":$ENV{LD_LIBRARY_PATH}";
     }
 
