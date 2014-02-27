cac3ed10b4c62efb9c7bade0580915b07717caaf
Philip Kime <Philip@kime.org.uk>
2014-02-27 09:59:11 +0100

    Cleaned up some internal utf8 stuff

--- btparse/src/format_name.c
+++ btparse/src/format_name.c
@@ -290,126 +290,6 @@ bt_set_format_options (bt_name_format * format,
    format->join_part[part] = join_part;
 }
 
-/* ------------------------------------------------------------------------
-@NAME       : get_uchar()
-@INPUT      : string
-              offset in string
-@OUTPUT     : number of bytes required to gobble the next unicode character, including any combining marks
-@RETURNS    : 
-@DESCRIPTION: In order to deal with unicode chars when calculating abbreviations,
-              we need to know how many bytes the next character is.
-@CALLS      : 
-@CALLERS    : count_virtual_char()
-@CREATED    : 2010/03/14, PK
-@MODIFIED   : 
--------------------------------------------------------------------------- */
-static int
-get_uchar(char * string, int offset)
-{
-  unsigned char * bytes = (unsigned char *)string;
-  int init;
-  unsigned int c = 0; // Without unsigned, for some reason Solaris coredumps
-
-  if(!string)
-    return 0;
-
-  if (     (// ASCII
-            bytes[offset] == 0x09 ||
-            bytes[offset] == 0x0A ||
-            bytes[offset] == 0x0D ||
-            (0x20 <= bytes[offset] && bytes[offset] <= 0x7E)
-            )
-           )
-    {
-      init = 1;
-    }
-
-  if(     (// non-overlong 2-byte
-           (0xC2 <= bytes[offset] && bytes[offset] <= 0xDF) &&
-           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0xBF)
-           )
-          )
-    {
-      init = 2;
-    }
-
-  if(     (// excluding overlongs
-           bytes[offset] == 0xE0 &&
-           (0xA0 <= bytes[offset+1] && bytes[offset+1] <= 0xBF) &&
-           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF)
-           ) ||
-          (// straight 3-byte
-           ((0xE1 <= bytes[offset] && bytes[offset] <= 0xEC) ||
-            bytes[offset] == 0xEE ||
-            bytes[offset] == 0xEF) &&
-           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0xBF) &&
-           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF)
-           ) ||
-          (// excluding surrogates
-           bytes[offset] == 0xED &&
-           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0x9F) &&
-           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF)
-           )
-          )
-    {
-      init = 3;
-    }
-
-  if(     (// planes 1-3
-           bytes[offset] == 0xF0 &&
-           (0x90 <= bytes[offset+1] && bytes[offset+1] <= 0xBF) &&
-           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF) &&
-           (0x80 <= bytes[offset+3] && bytes[offset+3] <= 0xBF)
-           ) ||
-          (// planes 4-15
-           (0xF1 <= bytes[offset] && bytes[offset] <= 0xF3) &&
-           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0xBF) &&
-           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF) &&
-           (0x80 <= bytes[offset+3] && bytes[offset+3] <= 0xBF)
-           ) ||
-          (// plane 16
-           bytes[offset] == 0xF4 &&
-           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0x8F) &&
-           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF) &&
-           (0x80 <= bytes[offset+3] && bytes[offset+3] <= 0xBF)
-           )
-          )
-    {
-      init = 4;
-    }
-
-  /* Now check for combining marks which are separate even in NFC */
-  while (bytes[offset+init+c]) {
-    /* 0300–036F - Combining Diacritical Marks */
-    if (  bytes[offset+init+c] == 0xCC &&
-          (0x80 <= bytes[offset+init+1+c] && bytes[offset+init+1+c] <= 0xAF)
-          )
-      {
-        c = c + 2; /* Skip to next possible combining mark */
-      }
-    /* 1DC0–1DFF - Combining Diacritical Marks Supplement */
-    else if (  bytes[offset+init+c] == 0xE1 &&
-               bytes[offset+init+1+c] == 0xB7 &&
-               (0x80 <= bytes[offset+init+2+c] && bytes[offset+init+2+c] <= 0xBF)
-               )
-      {
-        c = c + 3; /* Skip to next possible combining mark */
-      }
-    /* FE20–FE2F - Combining Half Marks */
-    else if (  bytes[offset+init+c] == 0xEF &&
-               bytes[offset+init+1+c] == 0xB8 &&
-               (0xA0 <= bytes[offset+init+2+c] && bytes[offset+init+2+c] <= 0xAF)
-               )
-      {
-        c = c + 3; /* Skip to next possible combining mark */
-      }
-    else {
-      break;
-    }
-  }
-  return init+c;
-}
-
 
 /* ----------------------------------------------------------------------
  * Functions for actually formatting a name (given a name and a name
@@ -452,9 +332,10 @@ count_virtual_char (char *    string,
                     int       offset, 
                     int *     vchar_count,
                     int *     depth,
-                    boolean * in_special,
-                    int *     utf8_length)
+                    boolean * in_special)
 {
+   int utf8_length = 0;
+
    switch (string[offset])
    {
       case '{': 
@@ -486,18 +367,18 @@ count_virtual_char (char *    string,
               only when we have a full character which could be multi-byte */
          {
            /* not tracking utf8 char yet, so start */ 
-           if (*utf8_length == 0)
-             *utf8_length = get_uchar(string, offset);
+           if (utf8_length == 0)
+             utf8_length = get_uchar(string, offset);
            /* Final byte in utf8 char so count this as a "character" */ 
-           if (*utf8_length == 1)
+           if (utf8_length == 1)
            {
              (*vchar_count)++;
-             *utf8_length = 0;
+             utf8_length = 0;
            }
            /* Inside a multi-byte utf-8 char so decrement the count as we move along
               the bytes */ 
-           if (*utf8_length > 1)
-             (*utf8_length)--;
+           if (utf8_length > 1)
+             utf8_length--;
          }
       }
    }
@@ -528,7 +409,6 @@ string_length (char * string)
    int      length;
    int      depth;
    boolean  in_special;
-   int      utf8_length;
    int      i;
 
    if (string == NULL)
@@ -537,11 +417,10 @@ string_length (char * string)
    length = 0;
    depth = 0;
    in_special = FALSE;
-   utf8_length = 0;
 
    for (i = 0; string[i] != 0; i++)
    {
-     count_virtual_char (string, i, &length, &depth, &in_special, &utf8_length);
+     count_virtual_char (string, i, &length, &depth, &in_special);
    }
 
    return length;
@@ -573,16 +452,14 @@ string_prefix (char * string, int prefix_start, int prefix_len)
    int     vchars_seen;
    int     depth;
    boolean in_special;
-   int      utf8_length;
 
    vchars_seen = 0;
    depth = 0;
    in_special = FALSE;
-   utf8_length = 0;
 
    for (i = prefix_start; string[i] != 0; i++)
    {
-     count_virtual_char (string, i, &vchars_seen, &depth, &in_special, &utf8_length);
+     count_virtual_char (string, i, &vchars_seen, &depth, &in_special);
       if (vchars_seen == prefix_len)
         return (i+1)-prefix_start;
    }
@@ -613,14 +490,12 @@ string_prefix_start (char * string, int index)
    int     vchars_seen;
    int     depth;
    boolean in_special;
-   int      utf8_length;
 
    vchars_seen = 0;
    depth = 0;
    in_special = FALSE;
-   utf8_length = 0;
 
-   count_virtual_char (string, index, &vchars_seen, &depth, &in_special, &utf8_length);
+   count_virtual_char (string, index, &vchars_seen, &depth, &in_special);
    if (! in_special && depth == 1)
      return index+1;
 
@@ -840,7 +715,6 @@ format_name (bt_name_format * format,
    int     vchars_seen;
    int     depth;
    boolean in_special;
-   int     utf8_length;
 
    /* 
     * Cull format->parts down by keeping only those parts that are actually
@@ -879,12 +753,11 @@ format_name (bt_name_format * format,
            vchars_seen = 0;
            depth = 0;
            in_special = FALSE;
-           utf8_length = 0;
 
            for (k = 0 ; tokens[part][j][k] != 0; k++)
            {
 
-             count_virtual_char (tokens[part][j], k, &vchars_seen, &depth, &in_special, &utf8_length);
+             count_virtual_char (tokens[part][j], k, &vchars_seen, &depth, &in_special);
              prefix_start = string_prefix_start (tokens[part][j], k);
 
              /* Add initial from the begining of the string or beginning of after-hyphen
--- btparse/src/util.c
+++ btparse/src/util.c
@@ -77,3 +77,123 @@ char *strupr (char *s)
    return s;
 }
 #endif
+
+/* ------------------------------------------------------------------------
+@NAME       : get_uchar()
+@INPUT      : string
+              offset in string
+@OUTPUT     : number of bytes required to gobble the next unicode character, including any combining marks
+@RETURNS    : 
+@DESCRIPTION: In order to deal with unicode chars when calculating abbreviations,
+              we need to know how many bytes the next character is.
+@CALLS      : 
+@CALLERS    : count_virtual_char()
+@CREATED    : 2010/03/14, PK
+@MODIFIED   : 
+-------------------------------------------------------------------------- */
+int
+get_uchar(char * string, int offset)
+{
+  unsigned char * bytes = (unsigned char *)string;
+  int init;
+  unsigned int c = 0; // Without unsigned, for some reason Solaris coredumps
+
+  if(!string)
+    return 0;
+
+  if (     (// ASCII
+            bytes[offset] == 0x09 ||
+            bytes[offset] == 0x0A ||
+            bytes[offset] == 0x0D ||
+            (0x20 <= bytes[offset] && bytes[offset] <= 0x7E)
+            )
+           )
+    {
+      init = 1;
+    }
+
+  if(     (// non-overlong 2-byte
+           (0xC2 <= bytes[offset] && bytes[offset] <= 0xDF) &&
+           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0xBF)
+           )
+          )
+    {
+      init = 2;
+    }
+
+  if(     (// excluding overlongs
+           bytes[offset] == 0xE0 &&
+           (0xA0 <= bytes[offset+1] && bytes[offset+1] <= 0xBF) &&
+           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF)
+           ) ||
+          (// straight 3-byte
+           ((0xE1 <= bytes[offset] && bytes[offset] <= 0xEC) ||
+            bytes[offset] == 0xEE ||
+            bytes[offset] == 0xEF) &&
+           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0xBF) &&
+           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF)
+           ) ||
+          (// excluding surrogates
+           bytes[offset] == 0xED &&
+           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0x9F) &&
+           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF)
+           )
+          )
+    {
+      init = 3;
+    }
+
+  if(     (// planes 1-3
+           bytes[offset] == 0xF0 &&
+           (0x90 <= bytes[offset+1] && bytes[offset+1] <= 0xBF) &&
+           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF) &&
+           (0x80 <= bytes[offset+3] && bytes[offset+3] <= 0xBF)
+           ) ||
+          (// planes 4-15
+           (0xF1 <= bytes[offset] && bytes[offset] <= 0xF3) &&
+           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0xBF) &&
+           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF) &&
+           (0x80 <= bytes[offset+3] && bytes[offset+3] <= 0xBF)
+           ) ||
+          (// plane 16
+           bytes[offset] == 0xF4 &&
+           (0x80 <= bytes[offset+1] && bytes[offset+1] <= 0x8F) &&
+           (0x80 <= bytes[offset+2] && bytes[offset+2] <= 0xBF) &&
+           (0x80 <= bytes[offset+3] && bytes[offset+3] <= 0xBF)
+           )
+          )
+    {
+      init = 4;
+    }
+
+  /* Now check for combining marks which are separate even in NFC */
+  while (bytes[offset+init+c]) {
+    /* 0300–036F - Combining Diacritical Marks */
+    if (  bytes[offset+init+c] == 0xCC &&
+          (0x80 <= bytes[offset+init+1+c] && bytes[offset+init+1+c] <= 0xAF)
+          )
+      {
+        c = c + 2; /* Skip to next possible combining mark */
+      }
+    /* 1DC0–1DFF - Combining Diacritical Marks Supplement */
+    else if (  bytes[offset+init+c] == 0xE1 &&
+               bytes[offset+init+1+c] == 0xB7 &&
+               (0x80 <= bytes[offset+init+2+c] && bytes[offset+init+2+c] <= 0xBF)
+               )
+      {
+        c = c + 3; /* Skip to next possible combining mark */
+      }
+    /* FE20–FE2F - Combining Half Marks */
+    else if (  bytes[offset+init+c] == 0xEF &&
+               bytes[offset+init+1+c] == 0xB8 &&
+               (0xA0 <= bytes[offset+init+2+c] && bytes[offset+init+2+c] <= 0xAF)
+               )
+      {
+        c = c + 3; /* Skip to next possible combining mark */
+      }
+    else {
+      break;
+    }
+  }
+  return init+c;
+}
